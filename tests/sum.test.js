e3076e594249e44568549788cead7b3ca47025c9
lrmiguel <leo.rmiguel@gmail.com>
2021-08-01 12:59:42 -0300

    feat: :sparkles: Add divide function

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,9 +1,9 @@
 const sum = require('../src/sum')
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3)
-// })
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
 
-// test('adds 12 + 2 to equal 14', () => {
-//   expect(sum(12, 2)).toBe(14)
-// })
+test('adds 12 + 2 to equal 14', () => {
+  expect(sum(12, 2)).toBe(14)
+})
